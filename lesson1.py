11d5ded1704f994822a2dbc5b6c56a4a3fdc4a7b
Bhumi <ahujabhumi12345@gmail.com>
2026-05-27 00:51:37 +0530

    Added lesson1

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+# lesson 1 - My First Python Program 
+name = input("Enter your name")
+age = int(input("Enter your age"))
+city = input("Enter your city")
+
+print("My name is " , name ,"I am " , age , "years old and I live in " , city )
